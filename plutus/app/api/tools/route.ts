c060f9461459c67030e0ac45ba8d2e82e45872da
Raunak Rose <raunakrose15@gmail.com>
2025-03-21 11:42:32 +0530

    merge main branch

---
+++ plutus/app/api/tools/route.ts
@@ -0,0 +1,39 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { SchemaType } from "@google/generative-ai/server";
+import { NextRequest, NextResponse } from "next/server";
+
+const genAI = new GoogleGenerativeAI(process.env.FLASH_API);
+
+const schema = {
+  description: "Identify the tool being called: checkBalance, SendETH, importWallet",
+  type: SchemaType.OBJECT,
+  properties: {
+    tool: {
+      type: SchemaType.STRING,
+      description: "Return name of the tool being called",
+      nullable: false,
+    },
+  },
+  required: ["tool"],
+}
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+  generationConfig: {
+    responseMimeType: "application/json",
+    responseSchema: schema,
+  }
+});
+
+
+export async function POST(req: NextRequest) {
+
+  const { value } = await req.json();
+  console.log(value);
+  const result = await model.generateContent(value);
+  console.log("Result: \n", result);
+  console.log("Response:", result.response);
+
+  return NextResponse.json(result.response.text());
+
+}
